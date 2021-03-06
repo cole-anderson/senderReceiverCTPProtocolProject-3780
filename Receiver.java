6fb9de2f5511328e58dac85b9323b3ed3231e6b6
coleanderson <spedits@gmail.com>
2021-03-05 19:40:31 -0700

    adding functionality to receiver

--- Receiver.java
+++ Receiver.java
@@ -5,19 +5,38 @@ public class Receiver {
          * possible command line arguments(args.length == 3 or 1) Receiver -f text.txt
          * port Receiver port
          */
+
+        // If command line argument does not contain a port
+        if (args.length < 2) {
+            System.out.println("No Port Specified, Exiting Program");
+            System.exit(0);
+        }
         // If -f command line argument present and host and port present
-        if (args[1] == "-f" && args.length == 3) {
+        else if (args[1] == "-f" && args.length == 3) {
             System.out.println("Data file selected for transmission: " + args[2]);
 
-        } else {
+        } else if (args.length == 2) {
             System.out.println("Message mode, no file specified");
+            String message = messageMode(args[2]);
+
         }
+
+        // SOCKET STUFF
     }
 
     /*
      * readFile: reads a textfile into an object
      */
-    void readFile(String fileName) {
+    static void readFile(String fileName) {
         //
+    }
+
+    static String messageMode(String port) {
+        String input = "";
+        System.out.println("Enter your message");
+        // TODO: add read here
+
+        return input;
+
     }
 }
