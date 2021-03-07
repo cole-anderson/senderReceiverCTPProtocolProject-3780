da00bfeeb3dc5f7eecba0d113b4532bde4b9440c
coleanderson <spedits@gmail.com>
2021-03-07 09:45:19 -0700

    just trying to fix parsing

--- Receiver.java
+++ Receiver.java
@@ -1,23 +1,34 @@
 public class Receiver {
     public static void main(String[] args) {
-        System.out.println("///args length test: " + args.length);
+        // TEST
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args at" + i + ":" + args[i]);
+        }
+        // Convert args to string which we then parse
+        for (int i = 0; i < args.length; i++) {
+
+        }
+
+        //
         /*
          * Receiver -f data-received.txt 64341 [OR] Receiver 64341
          */
 
         // If command line argument does not contain a port
-        if (args.length == 0 || args.length == 2) {
-            System.out.println("No Port Specified or incorrect commandline input, Exiting Program");
-            System.exit(0);
-        } else if (args.length == 1) {
-            System.out.println("No data file selected for receiving data, outputting to terminal");
+        // if (args.length == 0 || args.length == 2) {
+        // System.out.println("No Port Specified or incorrect commandline input, Exiting
+        // Program");
+        // System.exit(0);
+        // } else if (args.length == 1) {
+        // System.out.println("No data file selected for receiving data, outputting to
+        // terminal");
 
-        }
-        // If -f command line argument present and port is present
-        else if (args[1] == "-f") {
-            System.out.println("Data file selected for receiving data: " + args[2]);
+        // }
+        // // If -f command line argument present and port is present
+        // else if (args[0] == "-f") {
+        // System.out.println("Data file selected for receiving data: " + args[1]);
 
-        }
+        // }
         // TODO: SOCKET STUFF
     }
 
