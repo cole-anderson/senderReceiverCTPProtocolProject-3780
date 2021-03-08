bd3bc59830890a617330e9d010173e14dec101f2
coleanderson <spedits@gmail.com>
2021-03-07 21:11:12 -0700

    receiver fixed. now doing sender

--- Receiver.java
+++ Receiver.java
@@ -1,36 +1,29 @@
 public class Receiver {
     public static void main(String[] args) {
-        // TEST
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("args at" + i + ":" + args[i]);
-        }
-        // Convert args to string which we then parse
-        for (int i = 0; i < args.length; i++) {
-
-        }
-
-        //
         /*
          * Receiver -f data-received.txt 64341 [OR] Receiver 64341
          */
 
-        // If command line argument does not contain a port
-        // if (args.length == 0 || args.length == 2) {
-        // System.out.println("No Port Specified or incorrect commandline input, Exiting
-        // Program");
-        // System.exit(0);
-        // } else if (args.length == 1) {
-        // System.out.println("No data file selected for receiving data, outputting to
-        // terminal");
-
-        // }
-        // // If -f command line argument present and port is present
-        // else if (args[0] == "-f") {
-        // System.out.println("Data file selected for receiving data: " + args[1]);
-
-        // }
+        // (1)(EXAMPLE INPUT) Receiver -f data_received.txt 6431
+        if (args[0].equals("-f") && args.length == 2) {
+            System.out.println("Data file selected for receiving data: " + args[1]);
+        }
+        // (2)(EXAMPLE INPUT) Receiver 6431
+        else if (args.length == 1) {
+            System.out.println("No data file selected for receiving data. Outputting to terminal");
+        }
+        // (3)(SHOULD ACCOUNT FOR ALL INVALID INPUT)
+        else {
+            if (args.length == 0) {
+                System.out.println("No port specified. Exiting Program");
+                System.exit(0);
+            } else if (args.length > 2) {
+                System.out.println("Incorrect Commandline Input. Exiting Program");
+                System.exit(0);
+            }
+        }
         // TODO: SOCKET STUFF
-    }
+    }// ENDMAIN
 
     /*
      * writeFile: writes to textfile
--- Sender.java
+++ Sender.java
@@ -4,19 +4,19 @@ public class Sender {
          * sender -f data.txt 127.0.0.1 64341 [-f, file, ipaddress, port] ........
          * sender 127.0.0.1 64341 [ipaddress,port]
          */
-        // If command line argument does not contain a host and port
+        // If command line argument does not contain a host and port(exit)
         if (args.length <= 1) {
             System.out.println("No Port Specified, Exiting Program");
             System.exit(0);
         }
         // If -f command line argument present and host and port present
-        else if (args[1] == "-f" && args.length == 3) {
+        else if (args[1].equals("-f") && args.length == 3) {
             System.out.println("Data file selected for transmission: " + args[2]);
         }
         // If only host and port is present
         else if (args.length == 2) {
             System.out.println("Message mode, no file specified");
-            String message = messageMode();
+            // String message = messageMode();
             // args[2] = IP ADDRESS
             // args[3] = PORT
 
